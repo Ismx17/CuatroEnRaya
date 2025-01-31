a160921b4aa437aae1014a9976f356d33cb99c65
Ismx17 <irosros933@gmail.com>
2025-01-31 13:46:56 +0100

    MainApp arreglado para que asignar el color de ficha de forma manual

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/MainApp.java
@@ -1,23 +1,18 @@
 package org.iesalandalus.programacion.cuatroenraya;
 
+
 import org.iesalandalus.programacion.cuatroenraya.modelo.CuatroEnRaya;
-import org.iesalandalus.programacion.cuatroenraya.modelo.Ficha;
 import org.iesalandalus.programacion.cuatroenraya.modelo.Jugador;
 import org.iesalandalus.programacion.cuatroenraya.vista.Consola;
 
 public class MainApp {
+
     public static void main(String[] args) {
-        // Crear el primer jugador
+        System.out.println("Introduce los datos del PRIMER jugador.");
         Jugador jugador1 = new Jugador(Consola.leerNombre(), Consola.elegirColorFichas());
-        System.out.println(jugador1);
-
-        // Crear el segundo jugador con el color de ficha opuesto al primero
-        Jugador jugador2 = Consola.leerJugador(Ficha.VERDE);
-
-        // Crear una instancia del juego con ambos jugadores
-        CuatroEnRaya juego = new CuatroEnRaya(jugador1, jugador2);
-
-        // Iniciar el juego
+        System.out.println("Introduce los datos del SEGUNDO jugador.");
+        Jugador jugador2 = new Jugador(Consola.leerNombre(), Consola.elegirColorFichas());
+        CuatroEnRaya juego = new CuatroEnRaya(jugador1,jugador2);
         juego.jugar();
     }
 }
